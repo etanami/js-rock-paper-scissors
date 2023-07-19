e1238f4cc0e28dacdf68d5250b40d06c96051f8a
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-07-19 10:47:10 +0100

    add game function to find winner

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
+const choice = ["Rock", "Paper", "Scissors"];
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
 
 const getComputerChoice = () => {
   let randomNum = Math.floor(Math.random() * 3);
-  
-  if (randomNum === 0) {
-    return "rock";
-  } else if (randomNum === 1) {
-    return "paper";
+  return choice[randomNum];
+}
+
+
+const playRound = (playerSelection, computerSelection) => {
+
+  playerSelection = prompt("Pick your weapon of choice").toLowerCase();
+  computerSelection = getComputerChoice().toLowerCase();
+
+  console.log(computerSelection);
+
+  if(
+    (playerSelection == "rock" && computerSelection == "paper") || 
+    (playerSelection == "scissors" && computerSelection == "rock") || 
+    (playerSelection == "paper" && computerSelection == "scissors")) {
+    computerScore++;
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  } else if(playerSelection == computerSelection) {
+    return "Tie! No winner";
   } else {
-    return "scissors";
+    playerScore++;
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
   }
 }
 
 
-const playRound = (player, computer) => {
-  if(player == "rock" && computer == "scissors") {
-    return "You Win! Rock beats Scissors";
-  } else if(player == "rock" && computer == "paper") {
-    return "You Lose! Paper beats Rock";
+
+const game = () => {
+
+  for(let i = 0; i < 5; i++) {
+    let result = playRound(playerSelection, computerSelection);
+    console.log(result);
+  }
+
+  console.log(`playerScore: ${playerScore} - computerScore: ${computerScore}`)
+  if(playerScore > computerScore) {
+    return "I win!"
   } else {
-    return "Draw! No winner";
+    return "Computer wins!"
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+console.log(game())
